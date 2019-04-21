f4111f4ce03bb1609e6a75c596d051f5c7768df6
James G <james@peacefulprogramming.xyz>
2019-04-21 13:38:51 +0100

    change default pm2 script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   apps : [{
-    name: 'html-printer',
-    script: 'app.js',
+    name: 'puppeteer-browser',
+    script: 'puppeteer-browser.js',
     instance_var: "INSTANCE_ID",
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: 'one two',
-    instances: 4,
+    // args: 'one two',
+    instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
