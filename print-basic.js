6fb674ce455ab8af4cbf093dab1f8c70aec55df3
Ghost <goughjo02@gmail.com>
2019-04-22 18:08:55 +0100

    create high volumne testing method

--- print-basic.js
+++ print-basic.js
@@ -1,9 +1,9 @@
 const puppeteer = require('puppeteer');
 var path = require('path');
 
-const printBasic = function (firstName, lastName, email, size, pageSize) {
+const printBasic = function (firstName, lastName, email, size, pageSize, fileName) {
   return new Promise((resolve, reject) => {
-    console.log('printing basic');
+    // console.log('printing basic');
     let address = 'file:///' + path.resolve(__dirname, "build/basic/index.html");
     address = address + `?size=${size}&pageSize=${pageSize}`;
     address = address + `?firstName=${firstName}&lastName=${lastName}&email=${email}`;
@@ -22,7 +22,7 @@ const printBasic = function (firstName, lastName, email, size, pageSize) {
         }, e[0], e[1]))
       })
       const resolvedfinalArray = await Promise.all(editArray); // resolving all promises
-      const result = await page.pdf({ path: `./result.pdf`, format: 'A4' });
+      const result = await page.pdf({ path: `./example-results/${fileName}.pdf`, format: 'A4' });
       await page.close();
       await browser.disconnect();
       resolve(result);
